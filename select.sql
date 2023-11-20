b2488257dcc2ac3bf42473a3c6e00ff94a487ddf
Abderahim LAGRAOUI <abderahim.lagraoui@bordeaux-inp.fr>
2023-11-20 01:30:07 +0100

    requettes

--- select.sql
+++ select.sql
@@ -13,3 +13,9 @@ GROUP BY
 ORDER BY
     c.numero_client;
     
+-- Liste des modèles de véhicule pris en charge lors de l’année écoulée
+SELECT DISTINCT v.modele
+FROM vehicule v
+JOIN intervention i ON v.numero_immatricule = i.numero_intervention
+WHERE i.date_entree >= TO_DATE('2023-01-01', 'YYYY-MM-DD') 
+AND i.date_entree <= TO_DATE('2024-12-31', 'YYYY/MM/DD');
