9de027d9e212460a5db6417062049ede9edc01e5
Abderahim LAGRAOUI <abderahim.lagraoui@bordeaux-inp.fr>
2023-11-20 16:48:57 +0100

    jj

--- connect_pg.php
+++ connect_pg.php
@@ -1,6 +1,6 @@
 <?php
   $login = 'alagraoui001'   /*A compléter*/;
-  $db_pwd = 'lagraoui2022'  /*A compléter*/;
+  $db_pwd = 'lagraoui2020'  /*A compléter*/;
   /* Creation de l'objet qui gere la connexion: */
   $connection_string = "host=localhost port=5432 dbname=".$login." user=".$login." password=".$db_pwd;
   $connection = pg_connect($connection_string);
