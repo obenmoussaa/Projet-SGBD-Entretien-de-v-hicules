b8b39dff4ddd4e61783295fc1800ff783c1bca39
Abderahim LAGRAOUI <abderahim.lagraoui@bordeaux-inp.fr>
2023-12-04 17:14:11 +0100

    h

--- drop.sql
+++ drop.sql
@@ -1,8 +1,8 @@
 -- Suppression de la table proposer
-DROP TABLE IF EXISTS proposer_prescription;
+DROP TABLE IF EXISTS proposer_prescription  ;
 
 -- Suppression de la table faire
-DROP TABLE IF EXISTS garage_type_interventions;
+DROP TABLE IF EXISTS garage_type_interventions ;
 
 -- Suppression de la table prescription
 DROP TABLE IF EXISTS prescription CASCADE;
@@ -17,7 +17,7 @@ DROP TABLE IF EXISTS prescriptions_par_interventions CASCADE;
 
 
 -- Suppression de la table garage
-DROP TABLE IF EXISTS garage;
+DROP TABLE IF EXISTS garage CASCADE;  
 -- Suppression de la table client
 DROP TABLE IF EXISTS client CASCADE;
 
